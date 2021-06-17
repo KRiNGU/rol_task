b236d3a1abdb9bfe193c0b1fea7453accd8a6300
Alexandr Lobashov <viper.489@inbox.ru>
2021-06-17 18:56:03 +0300

    design ended

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,56 @@
 const cardTemplate = document.querySelector('.card-template').content;
 const cardList = document.querySelector(".cards");
 
-const initialCards = [ 
-    { 
-       name: 'qwe', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
-    { 
-       name: 'asd', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
-    { 
-       name: 'asd', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
-    { 
-       name: 'asd', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
-    { 
-       name: 'asd', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
-    { 
-       name: 'asd', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
-    { 
-       name: 'asd', 
-       link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
-    }, 
+const initialCards = [
+   { 
+      name: 'qwe', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
+   { 
+      name: 'asd', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
+   { 
+      name: 'asd', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
+   { 
+      name: 'asd', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
+   { 
+      name: 'asd', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
+   { 
+      name: 'asd', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
+   { 
+      name: 'asd', 
+      link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+   }, 
 ];
 
 initialCards.forEach(createCard);
 
 function createCard({ name, link }) {
-    const card = cardTemplate.querySelector('.card').cloneNode(true);
-    const likeBtn = card.querySelector('.card__like-button');
-    const deleteBtn = card.querySelector('.card__delete-button');
-    const cardTitle = card.querySelector('.card__title');
-    const cardFoto = card.querySelector('.card__foto');
-    cardTitle.textContent = name;
-    cardFoto.src = link;
-    deleteBtn.addEventListener('click', ()=>deleteElem(card));
-    likeBtn.addEventListener('click', ()=>toggleLike(likeBtn));
-    cardList.prepend(card);
+   const card = cardTemplate.querySelector('.card').cloneNode(true);
+   const likeBtn = card.querySelector('.card__like-button');
+   const deleteBtn = card.querySelector('.card__delete-button');
+   const cardTitle = card.querySelector('.card__title');
+   const cardFoto = card.querySelector('.card__foto');
+   cardTitle.textContent = name;
+   cardFoto.src = link;
+   deleteBtn.addEventListener('click', ()=>deleteElem(card));
+   likeBtn.addEventListener('click', ()=>toggleLike(likeBtn));
+   cardList.prepend(card);
 }
 
 function deleteElem(elem) {
-    elem.remove();
+   elem.remove();
 }
 
 function toggleLike(elem) {
-    elem.classList.toggle("card__like-button_active");
+   elem.classList.toggle("card__like-button_active");
 }
