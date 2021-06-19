3eb8beb1688ce6f01cf12084e02ed64606728319
Alexandr Lobashov <viper.489@inbox.ru>
2021-06-19 13:45:44 +0300

    added ability to edit user info (without avatar)

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,34 @@
 // Global scope
 
-// card constants
 const cardTemplate = document.querySelector('.card-template').content;
 const cardList = document.querySelector(".cards");
+
+// card constants
 const modalAddCard = document.querySelector(".modal_add-card");
 const modalAddCardCloseButton = modalAddCard.querySelector('.modal__close-button');
 const modalAddCardCreateButton = modalAddCard.querySelector('.modal__save-button');
 const modalAddCardCreateForm = modalAddCard.querySelector('.modal__form');
 const newCardName = modalAddCard.querySelector('.modal__input-title');
 const newCardPhotoUrl = modalAddCard.querySelector('.modal__input-url');
+
 // user constants
 let currentUser = document.querySelector('.user');
 const userName = currentUser.querySelector('.user__name');
 const userDescription = currentUser.querySelector('.user__description');
 const userAvatarUrl = currentUser.querySelector('.user__foto');
 
+// edit user modal
+const modalEditUser = document.querySelector('.modal_edit-user');
+const modalUserName = modalEditUser.querySelector('.modal__input-name');
+const modalUserDescription = modalEditUser.querySelector('.modal__input-description');
+const modalEditUserClosePopup = modalEditUser.querySelector('.modal__close-button');
+const modalEditUserSaveForm = modalEditUser.querySelector('.modal__form');
+
 // Initial constants
 
 const initialUser = { name: 'Alexander', 
-                     description: 'front-end junior',
-                     avatarUrl: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
+                     description: 'front-end junior'
                   };
-
 
 const initialCards = [
    {  
@@ -49,13 +56,24 @@ modalAddCardCreateForm.addEventListener('submit', function(event){
 
 // User events
 currentUser.querySelector('.user__add-card-button').addEventListener('click', ()=>openPopup(modalAddCard));
+currentUser.querySelector('.user__edit-info-button').addEventListener('click', ()=>openPopup(modalEditUser));
+
+modalEditUserClosePopup.addEventListener('click', ()=>closePopup(modalEditUser));
+
+modalEditUserSaveForm.addEventListener('submit', function(event){
+   event.preventDefault();
+   editUser({
+      name: modalUserName.value,
+      description: modalUserDescription.value
+   })
+   closePopup(modalEditUser);
+})
 
 // Functions
 
-function editUser({name, description, avatarUrl}) {
+function editUser({name, description}) {
    userName.textContent = name;
    userDescription.textContent = description;
-   userAvatarUrl.src = avatarUrl;
 }
 
 function openPopup(elem) {
