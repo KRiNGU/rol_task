2525c0f5abbede1171d16ed4ee930ed071afaa3f
Alexandr Lobashov <viper.489@inbox.ru>
2021-06-24 18:07:34 +0300

    fixed errors

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -14,27 +14,27 @@ export default class FormValidator {
     }
 
     _showInputError(input, message) {
-        let spanElement = input.nextSibling.nextSibling;
+        const spanElement = document.querySelector('.modal__input-error_' + input.id);
         spanElement.textContent = message;
         spanElement.classList.add('modal__input-error_shown');
     }
 
     _hideInputError(input) {
-        let spanElement = input.nextSibling.nextSibling;
+        const spanElement = document.querySelector('.modal__input-error_' + input.id);
         spanElement.textContent = '';
         spanElement.classList.remove('modal__input-error_shown');
     }
 
     _hasInvalidInput() {
-        return [...this.inputs].some((element) => element.validity.valid === false);
+        return this.inputs.some((element) => !element.validity.valid);
     }
 
     _toggleButtonState() {
         if (this._hasInvalidInput()) {
-            this.saveButton.setAttribute('disabled', 'disabled');
+            this.saveButton.disabled = true;
         }
         else {
-            this.saveButton.removeAttribute('disabled');
+            this.saveButton.disabled = false;
         }
     }
 
@@ -42,18 +42,18 @@ export default class FormValidator {
         this.form.addEventListener('submit', (event) => {
             event.preventDefault();
         })
-        for (let input of this.inputs) {
+        this.inputs.forEach((input)=>{
             input.addEventListener('input', (event) => {
                 console.log(input.validity.valid);
                 this._toggleButtonState();
                 this._checkInputValidity(event.target);
             })
-        }
+        })
         this._toggleButtonState();
     }
 
     enableValidation() {
-        this.inputs = this.form.querySelectorAll(this.selectorName);
+        this.inputs = [...this.form.querySelectorAll(this.selectorName)];
         this.saveButton = this.form.querySelector('.modal__save-button');
         this._setEventListeners();
     }
