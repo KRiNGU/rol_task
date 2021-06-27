195ace1654a036d4d9df17a8a3e67825535ba954
Alexandr Lobashov <viper.489@inbox.ru>
2021-06-27 03:48:44 +0300

    added and integrated UserInfo and deleted constants cause unused

--- scripts/constants.js
+++
@@ -1,6 +0,0 @@
-// global constants
-export const cardList = document.querySelector(".cards");
-// user constants
-export const currentUser = document.querySelector('.user');
-export const userName = currentUser.querySelector('.user__name');
-export const userDescription = currentUser.querySelector('.user__description');
--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,27 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
-import * as DOMconst from './constants.js';
 import PopupWithImage from './PopupWithImage.js';
 import PopupWithEdit from './PopupWithEdit.js';
+import UserInfo from './UserInfo.js';
 
 // Global scope
-const editUser = ({nickname, description})=>{
-   DOMconst.userName.textContent = nickname;
-   DOMconst.userDescription.textContent = description;
+const cardList = document.querySelector(".cards");
+const editUser = ({nickname, description, link})=>{
+   const userInfoDOMs = userInfo.getUserInfoDOMs();
+   userInfoDOMs.nickname.textContent = nickname;
+   userInfoDOMs.description.textContent = description;
+   userInfoDOMs.link.src = link;
 }
 
 const createCard = ({title, link})=>{
    const newCard = new Card(title, link, '.card-template', ()=>imagePopup.openImage(link, title)).getView();
-   DOMconst.cardList.prepend(newCard);
+   cardList.prepend(newCard);
 }
 
 // Initial constants
 const initialUser = { nickname: 'Alexander', 
-                     description: 'front-end junior'
+                     description: 'front-end junior',
+                     link: 'https://kubnews.ru/upload/iblock/aed/aede721d1ff8a00da41315253fc7aec7.jpg'
                     };
 
 const initialCards = [
@@ -30,6 +34,7 @@ const initialCards = [
 // Initiate page
 const editUserPopup = new PopupWithEdit('.modal__edit-user', editUser).getPopup();
 const createCardPopup = new PopupWithEdit('.modal__add-card', createCard).getPopup();
+const userInfo = new UserInfo(initialUser, editUserPopup, createCardPopup).getUserInfo();
 const imagePopup = new PopupWithImage().getPopup();
 const addCardValidator = new FormValidator('.modal__input', editUserPopup.getModal());
 addCardValidator.enableValidation();
@@ -38,7 +43,3 @@ editUserValidator.enableValidation();
 
 initialCards.forEach((card)=>createCard(card));
 editUser(initialUser);
-
-// User events
-DOMconst.currentUser.querySelector('.user__add-card-button').addEventListener('click', ()=>createCardPopup.openPopup());
-DOMconst.currentUser.querySelector('.user__edit-info-button').addEventListener('click', ()=>editUserPopup.openPopup());
